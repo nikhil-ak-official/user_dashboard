eb083ad07e9807af663d01f7d2aab50ac366456c
nikhil-ak-official <nikhilanilkumar@qburst.com>
2021-01-11 02:44:06 +0530

    logs

--- services/userServices.js
+++ services/userServices.js
@@ -295,7 +295,7 @@ const editUser = async (req, res) => {
                 })
                 console.log(req.role);
                 const role = req.role
-                const editedUser = updateUser[1][0].dataValues
+                const editedUser = updatedUser[1][0].dataValues
                 res.status(200).send({"success": 200, "message": "admin successfully edited user details", "data":{editedUser, role}})
             }
             }
