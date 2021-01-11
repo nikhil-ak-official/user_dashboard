ad6d0c20789e096269d31566c4f4d8b37c81c3fa
nikhil-ak-official <nikhilanilkumar@qburst.com>
2021-01-11 06:03:13 +0530

    logs

--- services/userServices.js
+++ services/userServices.js
@@ -318,8 +318,8 @@ const editUser = async (req, res) => {
                             updateUser[e] = req.body[e]
                         }
                     })
+                    console.log(updateUser);
                     const {id, createdAt, updatedAt, password, ...others} = updateUser.dataValues;
-                    console.log(others);
                     const editUser = await User.update(others, {
                         where: {
                             id: req.user.id
